28709aa0b9cdafe633cd9bac7e8e04bd9d91f371
janisfs <janisfs@mail.ru>
2024-08-10 14:42:09 +0300

    Update svetnewpars.py

--- svetpars/svetpars/spiders/svetnewpars.py
+++ svetpars/svetpars/spiders/svetnewpars.py
@@ -7,4 +7,10 @@ class SvetnewparsSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        pass
+        svets = response.css('div._Ud0k')
+        for svet in svets:
+            yield {
+                "name": svet.css("div.wYUX2 span::text").get(),
+                "price": svet.css("div.pY3d2 span::text").get(),
+                "url": svet.css("a").attrib["href"]
+            }
